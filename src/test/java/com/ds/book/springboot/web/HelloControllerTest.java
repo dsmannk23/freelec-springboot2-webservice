091a930391b163928b6d41b7d4a20d8c2505d3a5
dsmannk23 <dsmannk@gmail.com>
2020-08-18 23:26:11 +0900

    fixed untracked files

---
+++ src/test/java/com/ds/book/springboot/web/HelloControllerTest.java
@@ -0,0 +1,45 @@
+package com.ds.book.springboot.web;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.ResultMatcher;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(controllers = HelloController.class)
+public class HelloControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void hello가_리턴된다() throws Exception {
+        String hello = "hello";
+
+        RequestBuilder requestBuilder = MockMvcRequestBuilders.get("/hello");
+        mvc.perform(requestBuilder).andExpect(status().isOk()).andExpect(content().string(hello));
+    }
+
+    @Test
+    public void helloDto가_리턴된다() throws Exception {
+        String name = "hello";
+        int amount = 1000;
+
+        mvc.perform(MockMvcRequestBuilders.get("/hello/dto")
+                .param("name", name)
+                .param("amount", String.valueOf(amount)))
+                .andExpect(status().isOk())
+                .andExpect((ResultMatcher) jsonPath("$.name", is(name)))
+                .andExpect((ResultMatcher) jsonPath("$.amount", is(amount)));
+
+    }
+}
